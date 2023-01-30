46e4588b4daea8efcd26264dad5f1005a1bbe4d1
Pratibha-Knoldus <pratibha.yadav@knoldus.com>
2023-01-30 16:25:00 +0530

    first commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,55 @@
+import java.util.Objects;
+
+/**
+ * The type Student.
+ */
+public class Student {
+    private String name;
+
+    /**
+     * Instantiates a new Student.
+     *
+     * @param name the name
+     */
+    public Student(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/StudentListItem.java
@@ -0,0 +1,8 @@
+/**
+ * The type Student list item.
+ */
+public class StudentListItem {
+    private Student value;
+    private StudentListItem next;
+
+}
